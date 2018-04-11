ae9e68e73e8a19b9b81d3028b5d841ff66c0f9b8
giantObject <msclown1@naver.com>
2018-04-11 20:16:43 +0900

    학생 정보 수정 가능. 비밀번호 수정 작업 중

--- SchoolGUI/src/schoolGUI/StudentView.java
+++ SchoolGUI/src/schoolGUI/StudentView.java
@@ -13,55 +13,48 @@ import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class StudentView extends JFrame implements ActionListener{
+public class StudentView extends JFrame implements ActionListener {
 	String driver = "oracle.jdbc.driver.OracleDriver";
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	
+
 	String sql;
 	Connection con = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
-	
-	Font font;
-	
-	
-	
-	JLabel mainLabel,lb1,lb2,lb3,lb4,lb5,lb6,lb7,lb8;
-	
 
-	
-	
-	JButton updateBtn,completeBtn;
-	
-	JPanel mainPanel,panel0,panel1,panel2,panel3,panel4,panel5,panel6,panel7,panel8,updatePN;
-	
+	Font font;
+
+	JLabel mainLabel, lb1, lb2, lb3, lb4, lb5, lb6, lb7, lb8;
+
+	JButton updateBtn, completeBtn;
+
+	JPanel mainPanel, panel0, panel1, panel2, panel3, panel4, panel5, panel6, panel7, panel8, updatePN;
+
 	JTextField tf1 = new JTextField(10);
-	
+
 	JTextField tf2 = new JTextField(10);
-	
+
 	JTextField tf3 = new JTextField(10);
-	
+
 	JTextField tf4 = new JTextField(10);
-	
+
 	JTextField tf5 = new JTextField(10);
-	
+
 	JTextField tf6 = new JTextField(10);
-	
+
 	JTextField tf7 = new JTextField(10);
-	
+
 	JTextField tf8 = new JTextField(10);
-	
-	
-	
-	
+
 	public StudentView(ResultSet rs) {
 		super("학생 정보");
-		
+
 		this.rs = rs;
-		
+
 		font = new Font("돋움", Font.BOLD, 20);
 
 		mainPanel = new JPanel();
@@ -77,11 +70,10 @@ public class StudentView extends JFrame implements ActionListener{
 		panel7 = new JPanel();
 		panel8 = new JPanel();
 		updatePN = new JPanel();
-		
-		
+
 		mainLabel = new JLabel("프로필");
 		mainLabel.setFont(font);
-		
+
 		lb1 = new JLabel("id");
 		lb2 = new JLabel("학번");
 		lb3 = new JLabel("이름");
@@ -90,14 +82,13 @@ public class StudentView extends JFrame implements ActionListener{
 		lb6 = new JLabel("Email");
 		lb7 = new JLabel("전공");
 		lb8 = new JLabel("비밀번호");
-		
-		
+
 		updateBtn = new JButton("수정");
 		completeBtn = new JButton("완료");
-		
+
 		updatePN.add(updateBtn);
 		updatePN.add(completeBtn);
-		
+
 		tf1.setEditable(false);
 		tf2.setEditable(false);
 		tf3.setEditable(false);
@@ -107,34 +98,32 @@ public class StudentView extends JFrame implements ActionListener{
 		tf7.setEditable(false);
 		tf8.setEditable(false);
 
-		
-		
 		panel0.add(mainLabel);
-		
-		panel1.add(lb1);//id label
-		panel1.add(tf1);//textField
-		
-		panel2.add(lb2);//학번
+
+		panel1.add(lb1);// id label
+		panel1.add(tf1);// textField
+
+		panel2.add(lb2);// 학번
 		panel2.add(tf2);
-		
-		panel3.add(lb3);//이름
+
+		panel3.add(lb3);// 이름
 		panel3.add(tf3);
-		
-		panel4.add(lb4);//전화번호
+
+		panel4.add(lb4);// 전화번호
 		panel4.add(tf4);
-		
-		panel5.add(lb5);//주소
+
+		panel5.add(lb5);// 주소
 		panel5.add(tf5);
-		
-		panel6.add(lb6);//이메일
+
+		panel6.add(lb6);// 이메일
 		panel6.add(tf6);
-		
-		panel7.add(lb7);//전공
+
+		panel7.add(lb7);// 전공
 		panel7.add(tf7);
-		
-		panel8.add(lb8);//비밀번호
+
+		panel8.add(lb8);// 비밀번호
 		panel8.add(tf8);
-		
+
 		mainPanel.add(panel1);
 		mainPanel.add(panel2);
 		mainPanel.add(panel3);
@@ -143,77 +132,126 @@ public class StudentView extends JFrame implements ActionListener{
 		mainPanel.add(panel6);
 		mainPanel.add(panel7);
 		mainPanel.add(panel8);
-		
-		add(panel0,"North");
+
+		add(panel0, "North");
 		add(mainPanel);
-		add(updatePN,"South");
+		add(updatePN, "South");
 		setBounds(600, 200, 600, 600);
 		setVisible(true);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE );
-		
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 		updateBtn.addActionListener(this);
 		completeBtn.addActionListener(this);
-		
+
 		showInfo();
-		
+
 	}
-	
+
 	public void showInfo() {
 		connectDB();
-		
+
 		sql = "select * from Student";
 		try {
 			pstmt = con.prepareStatement(sql);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				 tf1.setText(rs.getString(1));
+
+			while (rs.next()) {
+				tf1.setText(rs.getString(1));
+				tf2.setText(rs.getString(2));
+				tf3.setText(rs.getString(3));
+				tf4.setText(rs.getString(4));
+				tf5.setText(rs.getString(5));
+				tf6.setText(rs.getString(6));
+				tf7.setText(rs.getString(7));
+				tf8.setText("**********");
+
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("쿼리문 보내기 실패");
 		}
 	}
-	
+
+	void insertInfo() {
+		try {
+			sql = "update student set name=?,phone=?,address=?,email=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, tf3.getText());
+			pstmt.setString(2, tf4.getText());
+			pstmt.setString(3, tf5.getText());
+			pstmt.setString(4, tf6.getText());
+
+			int result = pstmt.executeUpdate();
+			if (result == 1) {
+				JOptionPane.showMessageDialog(this, "Update success");
+			} else {
+				JOptionPane.showMessageDialog(this, "Update failed");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.toString();
+			System.out.println("failed Update");
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+					System.out.println("pstmt closed");
+				}
+				if (con != null) {
+					con.close();
+					System.out.println("con closed");
+				}
+			} catch (Exception e) {
+				e.toString();
+			}
+			showInfo();
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource().equals(updateBtn)) {
+		if (e.getSource().equals(updateBtn)) {
 			tf3.setEditable(true);
 			tf4.setEditable(true);
 			tf5.setEditable(true);
 			tf6.setEditable(true);
-			
-		}else if(e.getSource().equals(completeBtn)) {
-			
-			
-			
+
+		} else if (e.getSource().equals(completeBtn)) {
+			if (tf3.isEditable()) {
+				insertInfo();
+			}
+			tf3.setEditable(false);
+			tf4.setEditable(false);
+			tf5.setEditable(false);
+			tf6.setEditable(false);
+
 		}
 	}
-	
+
 	public void connectDB() {
 		try {
 			Class.forName(driver);
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println("드라이버 로딩 실패");
+			System.out.println("driver loading faied");
 		}
-		
+
 		try {
-			con = DriverManager.getConnection(url,"scott","tiger");
-			
+			con = DriverManager.getConnection(url, "scott", "tiger");
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println("커넥션 연결 실패");
+			System.out.println("connection failed");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		StudentView lg = new StudentView();
+		// StudentView lg = new StudentView();
 	}
 
 }
