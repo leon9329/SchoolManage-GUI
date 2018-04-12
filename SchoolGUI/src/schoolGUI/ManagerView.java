e9a691fd756689df3be2f9e45bea25f17b5149ea
giantObject <msclown1@naver.com>
2018-04-12 20:53:27 +0900

    매니저 초기화면 개발 중

--- SchoolGUI/src/schoolGUI/ManagerView.java
+++ SchoolGUI/src/schoolGUI/ManagerView.java
@@ -1,8 +1,18 @@
 package schoolGUI;
 
-public class ManagerView {
+import javax.swing.JFrame;
 
+public class ManagerView extends JFrame {
+
+	
+	
 	public ManagerView(){
 		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ManagerView mg = new ManagerView();
 	}
 }
